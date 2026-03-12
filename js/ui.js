4688f6dbb9825586a812667fbc54faffd30232bb
Nirmansutraa <nirmansutraa@gmail.com>
2026-03-12 21:17:36 +0530

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,40 +1,165 @@
+/* ------------------------------
+   LOAD LOGIN COMPONENT
+--------------------------------*/
+
+function renderLogin(){
+
+fetch("components/login.html")
+.then(res => res.text())
+.then(html => {
+
+document.getElementById("auth_layer").innerHTML = html
+document.getElementById("app_layer").innerHTML = ""
+
+})
+
+}
+
+
+/* ------------------------------
+   LOAD DASHBOARD
+--------------------------------*/
+
+function showDashboard(){
+
+fetch("components/dashboard.html")
+.then(res => res.text())
+.then(html => {
+
+document.getElementById("auth_layer").innerHTML = ""
+document.getElementById("app_layer").innerHTML = html
+
+})
+
+}
+
+
+/* ------------------------------
+   OPEN SURVEY MODULE
+--------------------------------*/
+
+function openSurvey(){
+
+fetch("components/survey.html")
+.then(res => res.text())
+.then(html => {
+
+document.getElementById("app_layer").innerHTML = html
+
+renderMaterials()
+renderFleet()
+
+})
+
+}
+
+
+/* ------------------------------
+   RENDER MATERIAL OPTIONS
+--------------------------------*/
+
 function renderMaterials(){
 
 let html=""
 
 materials.forEach(m=>{
 
-html+=`
+html += `
 
-<label>
+<label class="checkbox-item">
+
 <input type="checkbox" value="${m}">
+
 ${m}
+
 </label>
 
 `
 
 })
 
-document.getElementById("materials_list").innerHTML=html
+document.getElementById("materials_list").innerHTML = html
 
 }
+
+
+/* ------------------------------
+   RENDER FLEET OPTIONS
+--------------------------------*/
+
 function renderFleet(){
 
 let html=""
 
 fleetTypes.forEach(f=>{
 
-html+=`
+html += `
 
-<label>
+<label class="checkbox-item">
+
 <input type="checkbox" value="${f}">
+
 ${f}
+
 </label>
 
 `
 
 })
 
-document.getElementById("fleet_list").innerHTML=html
+document.getElementById("fleet_list").innerHTML = html
+
+}
+
+
+/* ------------------------------
+   COLLECT SELECTED MATERIALS
+--------------------------------*/
+
+function getSelectedMaterials(){
+
+let selected=[]
+
+document
+.querySelectorAll('#materials_list input:checked')
+.forEach(el=>{
+
+selected.push(el.value)
+
+})
+
+return selected.join(",")
+
+}
+
+
+/* ------------------------------
+   COLLECT SELECTED FLEET
+--------------------------------*/
+
+function getSelectedFleet(){
+
+let selected=[]
+
+document
+.querySelectorAll('#fleet_list input:checked')
+.forEach(el=>{
+
+selected.push(el.value)
+
+})
+
+return selected.join(",")
+
+}
+
+
+/* ------------------------------
+   LOGOUT
+--------------------------------*/
+
+function logout(){
+
+location.reload()
 
 }
