762516e9389bf6c552cd756a5b4eeddd592eab2c
Nirmansutraa <nirmansutraa@gmail.com>
2026-03-12 21:30:32 +0530

    Add GPS capture and reverse geocoding functionality
    Implement functions to capture GPS location and perform reverse geocoding.

--- js/gps.js
+++ js/gps.js
@@ -1,1 +1,74 @@
+/* ----------------------------------
+   CAPTURE GPS LOCATION
+---------------------------------- */
 
+function captureGPS(){
+
+if(!navigator.geolocation){
+
+alert("Geolocation not supported")
+return
+
+}
+
+navigator.geolocation.getCurrentPosition(
+
+position => {
+
+const lat = position.coords.latitude
+const lng = position.coords.longitude
+
+const gps = lat + "," + lng
+
+document.getElementById("gps_val").value = gps
+
+reverseGeocode(lat,lng)
+
+},
+
+error => {
+
+alert("Unable to capture location")
+
+},
+
+{
+enableHighAccuracy:true,
+timeout:10000,
+maximumAge:0
+}
+
+)
+
+}
+
+
+/* ----------------------------------
+   REVERSE GEOCODING
+---------------------------------- */
+
+async function reverseGeocode(lat,lng){
+
+try{
+
+const url =
+"https://nominatim.openstreetmap.org/reverse?format=json&lat="+lat+"&lon="+lng
+
+const res = await fetch(url)
+
+const data = await res.json()
+
+if(data.display_name){
+
+document.getElementById("business_address").value =
+data.display_name
+
+}
+
+}catch(err){
+
+console.log("Reverse geocode failed")
+
+}
+
+}
