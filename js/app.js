9c4e74a2ed6137a127624b148273d309c6c36d4a
Nirmansutraa <nirmansutraa@gmail.com>
2026-03-12 19:17:49 +0530

    Initialize InfraDepot system and load login interface
    Add initial setup for InfraDepot system with login interface.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
+document.addEventListener("DOMContentLoaded", startInfraDepot)
 
+function startInfraDepot(){
+
+console.log("InfraDepot system starting...")
+
+loadLogin()
+
+}
+
+/* Load login interface */
+
+function loadLogin(){
+
+fetch("components/login.html")
+.then(res => res.text())
+.then(html => {
+
+document.getElementById("auth_layer").innerHTML = html
+
+})
+
+}
