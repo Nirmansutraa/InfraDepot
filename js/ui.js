9eec1290bb98ccf547fc0adbde92361e8f5f3228
Nirmansutraa <nirmansutraa@gmail.com>
2026-03-12 21:15:09 +0530

    Add renderFleet function to display fleet types

--- js/ui.js
+++ js/ui.js
@@ -18,3 +18,23 @@ ${m}
 document.getElementById("materials_list").innerHTML=html
 
 }
+function renderFleet(){
+
+let html=""
+
+fleetTypes.forEach(f=>{
+
+html+=`
+
+<label>
+<input type="checkbox" value="${f}">
+${f}
+</label>
+
+`
+
+})
+
+document.getElementById("fleet_list").innerHTML=html
+
+}
