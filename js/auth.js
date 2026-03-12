d51ef8152efadff94e2e3874539b043b936da3a1
Nirmansutraa <nirmansutraa@gmail.com>
2026-03-12 19:29:08 +0530

    Implement login function and dashboard loading

--- js/auth.js
+++ js/auth.js
@@ -1,12 +1,50 @@
+/* LOGIN FUNCTION */
+
+async function login(){
+
+const id = document.getElementById("login_id").value
+const password = document.getElementById("login_pass").value
+
+const data = await apiRequest({
+action:"login",
+id:id,
+password:password
+})
+
+if(data.status === "OK"){
+
+showDashboard()
+
+}else{
+
+document.getElementById("login_status").innerText =
+"Login failed"
+
+}
+
+}
+
+
+/* LOAD DASHBOARD AFTER LOGIN */
+
 function showDashboard(){
 
 fetch("components/dashboard.html")
 .then(res => res.text())
 .then(html => {
 
-document.getElementById("auth_layer").innerHTML=""
-document.getElementById("app_layer").innerHTML=html
+document.getElementById("auth_layer").innerHTML = ""
+document.getElementById("app_layer").innerHTML = html
 
 })
+
+}
+
+
+/* LOGOUT */
+
+function logout(){
+
+location.reload()
 
 }
