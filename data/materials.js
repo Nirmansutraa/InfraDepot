588aac54e3fda3531fc003b72d8b726466314f88
Nirmansutraa <nirmansutraa@gmail.com>
2026-03-13 19:36:01 +0530

    Update materials.js

--- data/materials.js
+++ data/materials.js
@@ -1,10 +1,120 @@
-const materials=[
+const MATERIALS_DB = {
 
-"Sand",
-"Stone",
-"Aggregates",
-"Bricks",
-"Cement",
-"TMT Steel"
-
+Sand:{
+varieties:[
+"River Sand",
+"M-Sand",
+"Plaster Sand",
+"Pit Sand"
+],
+brands:[
+"Local River Source",
+"Washed Sand Plant",
+"Crusher Plant",
+"Premium Washed Sand"
 ]
+},
+
+Stone:{
+varieties:[
+"Building Stone",
+"Rubble Stone",
+"Boulder Stone",
+"Dressed Stone"
+],
+brands:[
+"Local Quarry",
+"Premium Quarry",
+"Granite Source",
+"Basalt Quarry"
+]
+},
+
+Aggregates:{
+varieties:[
+"6mm",
+"10mm",
+"20mm",
+"40mm",
+"Dust"
+],
+brands:[
+"L&T Crusher",
+"UltraStone",
+"BuildRock",
+"StoneMax",
+"RockSolid",
+"PrimeAggregates",
+"ShivStone",
+"MegaCrush",
+"InfraStone",
+"SuperGrit"
+]
+},
+
+Bricks:{
+varieties:[
+"Red Clay Brick",
+"Fly Ash Brick",
+"AAC Block",
+"Concrete Block"
+],
+brands:[
+"Magicrete",
+"Siporex",
+"UltraTech Xtralite",
+"Biltech",
+"JK Lakshmi Blocks",
+"HIL Blocks",
+"Brickwell",
+"MagicBlock",
+"BigBloc",
+"PrimeBlock"
+]
+},
+
+Cement:{
+varieties:[
+"OPC 43",
+"OPC 53",
+"PPC",
+"PSC",
+"White Cement"
+],
+brands:[
+"UltraTech",
+"ACC",
+"Ambuja",
+"Shree Cement",
+"Dalmia Bharat",
+"JK Lakshmi",
+"Birla Corporation",
+"Ramco Cement",
+"Orient Cement",
+"Wonder Cement"
+]
+},
+
+TMT:{
+varieties:[
+"Fe415",
+"Fe500",
+"Fe500D",
+"Fe550",
+"Fe600"
+],
+brands:[
+"Tata Tiscon",
+"JSW Neosteel",
+"SAIL",
+"Vizag Steel",
+"Jindal Panther",
+"Kamdhenu",
+"Shyam Steel",
+"SRMB",
+"Rathi",
+"Electrosteel"
+]
+}
+
+}
