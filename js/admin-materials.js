6f017845bc09aaf76274ec744672cb5946a116fa
Nirmansutraa <nirmansutraa@gmail.com>
2026-03-14 22:46:09 +0530

    Update admin-materials.js

--- js/admin-materials.js
+++ js/admin-materials.js
@@ -1,1 +1,17 @@
+export const MaterialIntelligence = {
+    // Top-tier mapping logic
+    hierarchy: {
+        "Cement": {
+            varieties: ["OPC 53", "PPC", "White Cement"],
+            brands: ["UltraTech", "Ambuja", "ACC", "Shree"]
+        },
+        "Steel": {
+            varieties: ["TMT 500D", "CRS", "Structural"],
+            brands: ["Tata Tiscon", "JSW Neosteel", "SAIL"]
+        }
+    },
 
+    saveNewBrand: async function(material, variety, brandName) {
+        // Firestore logic to add to 'brands' collection
+    }
+};
