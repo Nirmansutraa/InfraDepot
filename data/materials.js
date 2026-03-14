d0511943fc94046aefa8dfecbd12abb0fcbffb93
Nirmansutraa <nirmansutraa@gmail.com>
2026-03-14 21:54:31 +0530

    Update materials.js

--- data/materials.js
+++ data/materials.js
@@ -1,120 +1,21 @@
-const MATERIALS_DB = {
-
-Sand:{
-varieties:[
-"River Sand",
-"M-Sand",
-"Plaster Sand",
-"Pit Sand"
-],
-brands:[
-"Local River Source",
-"Washed Sand Plant",
-"Crusher Plant",
-"Premium Washed Sand"
-]
-},
-
-Stone:{
-varieties:[
-"Building Stone",
-"Rubble Stone",
-"Boulder Stone",
-"Dressed Stone"
-],
-brands:[
-"Local Quarry",
-"Premium Quarry",
-"Granite Source",
-"Basalt Quarry"
-]
-},
-
-Aggregates:{
-varieties:[
-"6mm",
-"10mm",
-"20mm",
-"40mm",
-"Dust"
-],
-brands:[
-"L&T Crusher",
-"UltraStone",
-"BuildRock",
-"StoneMax",
-"RockSolid",
-"PrimeAggregates",
-"ShivStone",
-"MegaCrush",
-"InfraStone",
-"SuperGrit"
-]
-},
-
-Bricks:{
-varieties:[
-"Red Clay Brick",
-"Fly Ash Brick",
-"AAC Block",
-"Concrete Block"
-],
-brands:[
-"Magicrete",
-"Siporex",
-"UltraTech Xtralite",
-"Biltech",
-"JK Lakshmi Blocks",
-"HIL Blocks",
-"Brickwell",
-"MagicBlock",
-"BigBloc",
-"PrimeBlock"
-]
-},
-
-Cement:{
-varieties:[
-"OPC 43",
-"OPC 53",
-"PPC",
-"PSC",
-"White Cement"
-],
-brands:[
-"UltraTech",
-"ACC",
-"Ambuja",
-"Shree Cement",
-"Dalmia Bharat",
-"JK Lakshmi",
-"Birla Corporation",
-"Ramco Cement",
-"Orient Cement",
-"Wonder Cement"
-]
-},
-
-TMT:{
-varieties:[
-"Fe415",
-"Fe500",
-"Fe500D",
-"Fe550",
-"Fe600"
-],
-brands:[
-"Tata Tiscon",
-"JSW Neosteel",
-"SAIL",
-"Vizag Steel",
-"Jindal Panther",
-"Kamdhenu",
-"Shyam Steel",
-"SRMB",
-"Rathi",
-"Electrosteel"
-]
-}
-
-}
+/**
+ * INFRA DEPOT - MATERIALS INTELLIGENCE DATA
+ */
+export const materialsData = {
+    "Sand": {
+        varieties: ["River Sand", "M Sand", "Plaster Sand", "Concrete Sand", "Fill Sand", "Desert Sand", "Pit Sand", "White Sand", "Washed Sand", "Crushed Sand"],
+        brands: ["Local River Supplier", "Crusher Plant Supplier", "Washed Sand Plant", "Mining Lease Operator", "Contract Supplier"]
+    },
+    "Stone & Aggregates": {
+        varieties: ["10mm Aggregate", "20mm Aggregate", "40mm Aggregate", "Crusher Dust", "Stone Chips", "Boulder", "Granite Aggregate", "Basalt Aggregate", "Limestone Aggregate", "Road Base Aggregate"],
+        brands: ["Local Crusher", "Mining Operator", "Quarry Owner", "Contract Supplier", "Wholesale Depot"]
+    },
+    "Cement": {
+        varieties: ["OPC 33 Grade", "OPC 43 Grade", "OPC 53 Grade", "PPC Cement", "PSC Cement", "White Cement", "Rapid Hardening", "Low Heat", "Sulphate Resistant", "Masonry Cement"],
+        brands: ["UltraTech", "ACC", "Ambuja", "Shree Cement", "JK Cement", "Dalmia", "Birla Cement", "Wonder Cement", "Nuvoco", "Ramco"]
+    },
+    "Steel": {
+        varieties: ["TMT Bars", "Structural Steel", "Binding Wire", "MS Rods", "MS Angles", "MS Channels", "MS Pipes", "GI Pipes", "Steel Sheets", "Wire Rods"],
+        brands: ["Tata Tiscon", "JSW Steel", "SAIL", "Jindal Steel", "Kamdhenu", "SRMB", "Shyam Steel", "Vizag Steel", "APL Apollo", "Rashmi"]
+    }
+};
