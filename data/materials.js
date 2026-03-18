6bee4cdf97018218f0a2792c91fca33300d2f55c
Nirmansutraa <nirmansutraa@gmail.com>
2026-03-18 22:53:47 +0530

    Update materials.js

--- data/materials.js
+++ data/materials.js
@@ -1,21 +1,26 @@
-/**
- * INFRA DEPOT - MATERIALS INTELLIGENCE DATA
- */
-export const materialsData = {
-    "Sand": {
-        varieties: ["River Sand", "M Sand", "Plaster Sand", "Concrete Sand", "Fill Sand", "Desert Sand", "Pit Sand", "White Sand", "Washed Sand", "Crushed Sand"],
-        brands: ["Local River Supplier", "Crusher Plant Supplier", "Washed Sand Plant", "Mining Lease Operator", "Contract Supplier"]
-    },
-    "Stone & Aggregates": {
-        varieties: ["10mm Aggregate", "20mm Aggregate", "40mm Aggregate", "Crusher Dust", "Stone Chips", "Boulder", "Granite Aggregate", "Basalt Aggregate", "Limestone Aggregate", "Road Base Aggregate"],
-        brands: ["Local Crusher", "Mining Operator", "Quarry Owner", "Contract Supplier", "Wholesale Depot"]
-    },
-    "Cement": {
-        varieties: ["OPC 33 Grade", "OPC 43 Grade", "OPC 53 Grade", "PPC Cement", "PSC Cement", "White Cement", "Rapid Hardening", "Low Heat", "Sulphate Resistant", "Masonry Cement"],
-        brands: ["UltraTech", "ACC", "Ambuja", "Shree Cement", "JK Cement", "Dalmia", "Birla Cement", "Wonder Cement", "Nuvoco", "Ramco"]
-    },
-    "Steel": {
-        varieties: ["TMT Bars", "Structural Steel", "Binding Wire", "MS Rods", "MS Angles", "MS Channels", "MS Pipes", "GI Pipes", "Steel Sheets", "Wire Rods"],
-        brands: ["Tata Tiscon", "JSW Steel", "SAIL", "Jindal Steel", "Kamdhenu", "SRMB", "Shyam Steel", "Vizag Steel", "APL Apollo", "Rashmi"]
-    }
+const MATERIALS = {
+Sand: {
+  varieties: ["River Sand","M Sand","Plaster Sand"],
+  brands: ["Local Supplier","Ultra Sand","Robo Sand"]
+},
+Aggregates: {
+  varieties: ["10mm","20mm","40mm"],
+  brands: ["Tata Aggregates","UltraStone","Local Crusher"]
+},
+"Masonry Stone": {
+  varieties: ["Granite","Limestone","Sandstone"],
+  brands: ["Jodhpur Stone","Udaipur Stone","Local Quarry"]
+},
+"TMT Steel": {
+  varieties: ["Fe500","Fe550","Fe600"],
+  brands: ["Tata Tiscon","JSW Steel","Kamdhenu"]
+},
+Cement: {
+  varieties: ["OPC","PPC","White Cement"],
+  brands: ["UltraTech","ACC","Ambuja"]
+},
+Bricks: {
+  varieties: ["Red Brick","Fly Ash","AAC Block"],
+  brands: ["Local Kiln","Magicrete","Siporex"]
+}
 };
